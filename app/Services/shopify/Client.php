b95575fdc2027261f3f22c52fbe34266033ecc01
danil zakablukovskii <danil.kabluk@gmail.com>
2015-08-04 18:35:29 +0200

    fix class name fix exception

--- app/Services/shopify/Client.php
+++ app/Services/shopify/Client.php
@@ -188,7 +188,7 @@ class Client
     private function shopApiCallLimitParam($index)
     {
         if ($this->last_response_headers == null) {
-            throw new Exception('Cannot be called before an API call.');
+            throw new \Exception('Cannot be called before an API call.');
         }
         $params = explode('/', $this->last_response_headers['http_x_shopify_shop_api_call_limit']);
 
